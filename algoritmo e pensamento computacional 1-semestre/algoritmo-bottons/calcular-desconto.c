e4667cec7cc68b7886b4e6389237f541d86bad49
PAULO HENRIQUE NASCIMENTO QUEIROZ <paulo.queiroz006@cs.unicid.edu.br>
2025-09-04 10:10:59 -0300

    Codigo simples para calcular desconto de um produto

---
+++ algoritmo e pensamento computacional 1-semestre/algoritmo-bottons/calcular-desconto.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    float valor_unitario, quantidade, desconto, valor_excedente, valor_descontado, valor_total;
+    printf("Digite o valor de cada unidade do produto: ");
+    scanf("%f", &valor_unitario);
+    
+    printf("Digite o valor do desconto : ");
+    scanf("%f", &desconto);
+    desconto = desconto / 100;
+    
+    printf("Digite a quantidade vendida: ");
+    scanf("%f", &quantidade);
+
+    if (quantidade <= 100) {
+        valor_total = quantidade * valor_unitario;
+        printf("Valor total a pagar: R$ %.2f\n", valor_total);
+        return 0;
+    }
+    else {
+        valor_excedente = quantidade - 100;
+        valor_descontado = (valor_excedente * valor_unitario) * desconto;
+        valor_total = (100 * valor_unitario) + (valor_excedente * valor_unitario) - valor_descontado;
+        printf("Valor total a pagar com desconto: R$ %.2f\n", valor_total); 
+        return 0;
+    }
+}
