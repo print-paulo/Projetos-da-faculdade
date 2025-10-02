29d7e01ab9a6162a8499a80934040117e4a4b667
Paulo Henrique <paulohenriquenscmnt06@gmail.com>
2025-10-01 21:41:21 -0300

    Feito a função 'calcularAlfabetoPulado'

--- Algoritmo-e-Pensamento-Computacional/1°-Semestre/atividade-de-decriptografia/decriptografar.c
+++ Algoritmo-e-Pensamento-Computacional/1°-Semestre/atividade-de-decriptografia/decriptografar.c
@@ -13,13 +13,28 @@ void aplicarShift(char *alfabetoShiftado, int shift) {
     }
 
     strncpy(alfabetoShiftado, alfabeto + shift, 26 - shift);
+    alfabetoShiftado[26 - shift] = '\0';
     strncat(alfabetoShiftado, alfabeto, shift);
+}
 
+void calcularAlfabetoPulado(char* alfabetoPulado, const char* alfabetoShiftado, int pulo) {
+    int usado[26] = {0};
+
+    for (int i = pulo - 1; i < 26; i += pulo) {
+        strncat(alfabetoPulado, &alfabetoShiftado[i], 1);
+        usado[i] = 1;
+    }
 }
 
 int main () {
     char textoCriptografado[32] = "";
+    int shift = 0;
 
     printf("Digite o texto a ser decriptografada: \n");
     scanf("%s", textoCriptografado);
+
+    printf("Digite o valor do shift: \n");
+    scanf("%d", &shift);
+
+
 }
